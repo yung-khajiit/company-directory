aec10e2746648d6cb8a64fb04b7f1ae078cff483
yung-khajiit <jeenriquez1219@gmail.com>
2024-02-08 08:13:31 -0600

    Fixed Rounded Corners on Image and Card

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -12,7 +12,7 @@ const username = faker.internet.userName({ firstName: firstname, lastName: lastn
 <template>
 <!-- {{ fullname }} <br/> -->
     <div class="border border-gray-400 rounded-lg shadow bg-gray-200 h-full">
-        <img :src="faker.image.urlLoremFlickr({category: 'cats'})" />
+        <img :src="faker.image.urlLoremFlickr({category: 'cats'})" class="rounded-t-xl" />
         <div class="p-4">
             <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
             <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
