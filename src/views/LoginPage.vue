0f96ad28a31bbb4da254d39167863e6885df172d
yung-khajiit <jeenriquez1219@gmail.com>
2024-02-14 08:20:14 -0600

    Styled Login Page

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,16 +1,16 @@
 <template>
-    <form class="" @submit.prevent>
+    <form class="m-10" @submit.prevent>
         <div>
-            <label for="username" class="">Usernamer</label>
+            <label for="username" class="block mb-2 font-bold">Usernamer</label>
             <input id="username" v-model="username" type="text" placeholder="Username">
         </div>
 
         <div>
-            <label for="password" class="">Password</label>
+            <label for="password" class="block mb-2 font-bold">Password</label>
             <input id="password" v-model="password" type="password" placeholder="Password">
         </div>
 
-        <button @click="logUserIn" class="">Login</button>
+        <button @click="logUserIn" class="bg-green-500 mt-4 px-4 py-2 hover:bg-green-800 hover:text-white">Login</button>
     </form>
 
 </template>
